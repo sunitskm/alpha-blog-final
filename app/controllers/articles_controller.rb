1bc487d2fe82cf9a9d1bb5759f9657a086a28848
skm alpha blog <sunitmishra0110@gmail.com>
2017-10-09 00:15:46 +0000

    Add edit update show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,18 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -16,6 +28,7 @@ class ArticlesController < ApplicationController
    # redirect_to article_path(@article)
   end
   def index
+    @articles = Article.all
   end
   def show
     @article = Article.find(params[:id])
